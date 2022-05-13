5383b16194bea3a657fd4fd2538a4d2fc0be8e16
Neb <nex.shaw.jobs@gmail.com>
2022-05-13 12:05:14 -0400

    more simple APIs

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,41 @@
 import Vapor
 
-func routes(_ app: Application) throws {
-    app.get { req in
-        return req.view.render("index", ["title": "Hello Vapor!"])
-    }
+struct VideoCourse: Content {
+    let name: String
+    let language: String
+    let version: Double
+}
+struct Echo: Content {
+    let clientText: String
+    let timeStamp: String
+}
 
-    app.get("hello") { req -> String in
-        return "Hello, world!"
+struct EchoResponse: Content {
+    let clientTextRequested: String
+    let timeStapSent: String
+}
+func routes(_ app: Application) throws {
+//    app.get { req in
+//        return req.view.render("index", ["title": "Hello Vapor!"])
+//    }
+//
+//    app.get("hello") { req -> String in
+//        return "Hello, world!"
+//    }
+    
+    app.post("upload") { req -> HTTPResponseStatus in
+        let course = try req.content.decode(VideoCourse.self)
+        print("course name \(course.name)")
+        print("course launguage \(course.language)")
+        print("course version \(course.version)")
+        return .ok
+        
+    }
+    
+    app.post("echo") { req -> EchoResponse in
+        let echo = try req.content.decode(Echo.self)
+        print("You typed \(echo.clientText)")
+        print("at time \(echo.timeStamp)")
+        return EchoResponse(clientTextRequested: "You said \(echo.clientText)", timeStapSent: "at \(echo.timeStamp)")
     }
 }
